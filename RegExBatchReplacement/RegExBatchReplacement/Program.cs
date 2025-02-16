6de444d372efca948bbb2221bedbc25cd153a02d
ДеЮра <doomov@yandex.ru>
2025-02-16 08:06:32 +0700

    Add files via upload

---
+++ RegExBatchReplacement/RegExBatchReplacement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegExBatchReplacement
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string Text = "1 Onee, 2 Two, 33 Throoee is good.";
+			IList<string> pattern = [ @"\d+", "ee", "oo" ];
+			IList<string> output = [ "Цифра", "ёBbb-т", "OOO-T" ];
+
+			if (pattern.Count == output.Count)
+			{
+				for (int i = 0; i < output.Count; i++)
+				{
+					var rx = new Regex(pattern[i], RegexOptions.Compiled | RegexOptions.IgnoreCase);
+					Text = rx.Replace(Text, output[i]);
+				}
+				Console.WriteLine("Результат:\r\n" + Text);
+			}
+			else
+				Console.WriteLine("Ошибка:\r\nКоличество pattern и замен не совпадает!");
+
+			Console.ReadKey();
+		}
+	}
+}
